b4011527faa9257c65f50fe5e73a8f82c59b0779
Andrew McCarthy <andrewmcc1120@gmail.com>
2024-02-14 21:47:29 -0500

    faq page build out

---
+++ pages/faq.js
@@ -0,0 +1,28 @@
+import data from './models/faq-list.object.json' assert { type: 'json' }
+
+window.onload = (event) => {
+  generateList(data);
+}
+
+function generateList(faqList) {
+  let listContainer = document.getElementById('accordion-container');
+  faqList.forEach(faqItem => {
+    console.log(faqItem);
+    let accordionItem = document.createElement('div');
+    accordionItem.classList.add('accordion-item');
+    accordionItem.innerHTML = `
+    <h2 class="accordion-header">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+      data-bs-target="#collapse${faqItem.ID}" aria-expanded="false" aria-controls="collapse${faqItem.ID}">
+      ${faqItem.Question}
+    </button>
+  </h2>
+  <div id="collapse${faqItem.ID}" class="accordion-collapse collapse" data-bs-parent="#accordion-container">
+    <div class="accordion-body">
+    ${faqItem.Answer}
+    </div>
+  </div>
+    `;
+    listContainer.append(accordionItem);
+  });
+}
