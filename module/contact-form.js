b3b19dd08b1e11dc97bb288c29fc215c9c6eb7e8
Andrew McCarthy <andrewmcc1120@gmail.com>
2024-04-13 11:11:25 -0400

    form validation, email body, adjusted success message

--- module/contact-form.js
+++ module/contact-form.js
@@ -9,17 +9,17 @@ function generateContactForm() {
     <div class="row pb-3">
       <div class="col-md-6">
         <label for="fullNameControl" class="form-label">Full Name</label>
-        <input type="text" class="form-control" id="fullNameControl">
+        <input type="text" class="form-control" id="fullNameControl" name="fullName" required>
       </div>
       <div class="col-md-6">
         <label for="emailControl" class="form-label">Email Address</label>
-        <input type="email" class="form-control" id="emailControl">
+        <input type="email" class="form-control" id="emailControl" name="email" required>
       </div>
     </div>
     <div class="row mb-3">
       <div class="col">
         <Label for="messageControl" class="form-label">Message</Label>
-        <textarea class="form-control" rows="6" id="messageControl"></textarea>
+        <textarea class="form-control" rows="6" id="messageControl" name="message" required></textarea>
       </div>
     </div>
     <div class="row">
@@ -37,15 +37,41 @@ function generateContactForm() {
 
 function sendEmail(event) {
   event.preventDefault();
+  let contactForm = document.querySelector('form');
+  let fullName = contactForm.fullName.value;
+  let emailAddress = contactForm.email.value;
+  let message = contactForm.message.value;
   Email.send({
     SecureToken : "ef7cbd87-0d4c-4dba-8864-7cbe283f238d",
     To : "andrewmcc1120@gmail.com",
     From : "workbench@apguitars.com",
-    Subject : "This is a test email",
-    Body : "This test is being sent from the contact form. Please announce its delivery."
+    Subject : "Contact Form Submission",
+    Body : `Name: ${fullName} <br/> email address: ${emailAddress} <br/> ${message}`
 }).then(
-  message => alert(message)
+  message => alert(adjustMessage(message)),
+  contactForm.reset()
 );
 }
 
-// SMTPJS and smtp.gmail server
+function adjustMessage(message) {
+  if (message === "OK") {
+    // sendConfirmationEmail();
+    return message = "Thank you for your email!";
+  } else {
+    return message;
+  }
+}
+
+// function sendConfirmationEmail() {
+//   let contactForm = document.querySelector('form');
+//   let emailAddress = contactForm.email.value;
+//   Email.send({
+//     SecureToken : "ef7cbd87-0d4c-4dba-8864-7cbe283f238d",
+//     To : `${emailAddress}`,
+//     From : "workbench@apguitars.com",
+//     Subject : "Contact Form Submission",
+//     Body : "Thank you for reaching out! I'll be in contact with you as soon as possible. Don't hesitate to add more"
+// });
+// }
+
+// SMTPJS and elasticEmail server
